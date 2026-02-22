6bd46456d6a9901f493cf46296b608493f3dda20
EdsonArantes23 <126924136+EdsonArantes23@users.noreply.github.com>
2026-02-22 22:10:24 +0300

    config.py

--- config.py
+++ config.py
@@ -4,11 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+OPENROUTER_KEY = os.getenv("OPENROUTER_KEY")
 ADMIN_ID = int(os.getenv("ADMIN_ID"))
-
 DIGEST_HOUR = int(os.getenv("DIGEST_HOUR", 20))
 DIGEST_MINUTE = int(os.getenv("DIGEST_MINUTE", 0))
-
 SPONSOR_LINK = os.getenv("SPONSOR_LINK", "")
 SPONSOR_TEXT = os.getenv("SPONSOR_TEXT", "")
